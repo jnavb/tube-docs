2753ecd1654db0fa273d91a0b81bd1487cb4a1ba
Jorge Navarro <jorgenavb@gmail.com>
2021-05-17 12:02:05 +0200

    feat: add csv example

--- src/ace/mode-tube.js
+++ src/ace/mode-tube.js
@@ -44,7 +44,7 @@ const defineTubeModeHighlightRules = () =>
             },
             {
               token: 'variable.parameter.prefixes',
-              regex: /(than|with|without|for|between|by|at|to|until|and|below|under|on|since|ago|past|into|from|about|through|across|after)(?=\s|$)/,
+              regex: /(than|with|without|for|between|by|at|to|until|and|or|below|under|on|since|ago|past|into|from|about|through|across|after)(?=\s|$)/,
             },
             {
               token: 'keyword.alias',
