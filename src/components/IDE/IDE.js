459eb4de31b9d5029a37dc7e60b387acd2ca4864
Jorge Navarro <jorgenavb@gmail.com>
2021-05-20 09:10:22 +0200

    fix: cosmetics at IDE tabs

--- src/components/IDE/IDE.js
+++ src/components/IDE/IDE.js
@@ -107,7 +107,7 @@ const Tab = styled.button`
   color: white;
   font-size: 18px;
   cursor: pointer;
-  border-top: 6px solid ${props => (props.isDarkThemeActive ? '#132436' : 'white')};
+  border-top: 6px solid #132436;
   font-weight: 600;
   color: #abb2bf;
 
